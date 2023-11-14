996924ab4500501d7c4d006ff72ea347246635b1
Heejun Byeon <hejuby@gmail.com>
2023-11-14 17:48:46 +0900

    feat: print header message of the reward page

--- src/OutputView.js
+++ src/OutputView.js
@@ -4,6 +4,9 @@ export default OutputView = {
   printGreeting() {
     Console.print("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
   },
+  printRewardHeader(day) {
+    Console.print(`12월 ${day}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!`);
+  },
   printMenu(orders) {
     Console.print("<주문 메뉴>");
     orders.map((order) => {
