4a3b25e35459e867e58b3fb4e4f45ad0745672f7
Heejun Byeon <hejuby@gmail.com>
2023-11-14 17:47:17 +0900

    feat: print ordered menus

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,11 +1,13 @@
 import { Console } from '@woowacourse/mission-utils';
 
 export default OutputView = {
-  printMenu() {
-    Console.print("<주문 메뉴>");
-    // ...
-  }, 
   printGreeting() {
     Console.print("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+  },
+  printMenu(orders) {
+    Console.print("<주문 메뉴>");
+    orders.map((order) => {
+      Console.print(`${order[0]} ${order[1]}개`);
+    })
   }
 }
